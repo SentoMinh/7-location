e0edcf4e95706a5ecc0d3e8ede0830c4ebefdec2
SentoMinh <92153096+SentoMinh@users.noreply.github.com>
2023-04-12 08:10:02 +0200

    Add files via upload

---
+++ teamwork/Untitled-1.py
@@ -0,0 +1,7 @@
+works =[ ["name","scname",pernum,ezernum] , ["name","scname",pernum,ezernum] ]
+
+for i in range(0, len(works)):
+    print(i+1," ",works[i][0])
+#
+valasz=input("Melyiket valasztod?  Add meg a számjelét! ")
+valasz_index=int(valasztas)-1
